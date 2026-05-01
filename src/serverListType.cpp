0ad5708d97417a800ff125b997da0c928ca01843
Seif-rashwan <seifeldin.rashwan@ejust.edu.eg>
2026-05-01 19:21:29 +0300

    style: fix code formatting issues in main.cpp, serverType.cpp, and serverListType.cpp

--- src/serverListType.cpp
+++ src/serverListType.cpp
@@ -3,45 +3,58 @@
 
 using namespace std;
 
-ServerListType::ServerListType(int num) {
+ServerListType::ServerListType(int num)
+{
     numOfServers = num;
     servers = new ServerType[numOfServers];
 }
 
-ServerListType::~ServerListType() {
+ServerListType::~ServerListType()
+{
     delete[] servers;
 }
 
-int ServerListType::getFreeServerID() const {
-    for (int i = 0; i < numOfServers; i++) {
-        if (servers[i].isFree()) {
+int ServerListType::getFreeServerID() const
+{
+    for (int i = 0; i < numOfServers; i++)
+    {
+        if (servers[i].isFree())
+        {
             return i;
         }
     }
     return -1; 
 }
 
-int ServerListType::getNumberOfBusyServers() const {
+int ServerListType::getNumberOfBusyServers() const
+{
     int busyServers = 0;
-    for (int i = 0; i < numOfServers; i++) {
-        if (!servers[i].isFree()) {
+    for (int i = 0; i < numOfServers; i++)
+    {
+        if (!servers[i].isFree())
+        {
             busyServers++;
         }
     }
     return busyServers;
 }
 
-void ServerListType::setServerBusy(int serverID, const CustomerType& cCustomer) {
+void ServerListType::setServerBusy(int serverID, const CustomerType& cCustomer)
+{
     servers[serverID].setBusy();
     servers[serverID].setCurrentCustomer(cCustomer);
     servers[serverID].setTransactionTime();
 }
 
-void ServerListType::updateServers() {
-    for (int i = 0; i < numOfServers; i++) {
-        if (!servers[i].isFree()) {
+void ServerListType::updateServers()
+{
+    for (int i = 0; i < numOfServers; i++)
+    {
+        if (!servers[i].isFree())
+        {
             servers[i].decreaseTransactionTime();
-            if (servers[i].getRemainingTransactionTime() == 0) {
+            if (servers[i].getRemainingTransactionTime() == 0)
+            {
                 cout << "Server No: " << (i + 1) << " is now free." << endl;
                 servers[i].setFree();
             }
