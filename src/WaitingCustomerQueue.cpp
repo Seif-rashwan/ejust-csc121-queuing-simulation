115966959a1995d58919642b09b16db614789133
andrew <androw.shonuda2002@gmail.com>
2026-05-06 15:08:56 +0300

    Update WaitingCustomerQueue.cpp

--- src/WaitingCustomerQueue.cpp
+++ src/WaitingCustomerQueue.cpp
@@ -1,106 +1,89 @@
 #include "WaitingCustomerQueue.h"
 #include <cassert>
 #include <iostream>
-using std::cout;
-using std::endl;
 
-// 1. التحقق لو الطابور فاضي
 template <class Type>
 bool WaitingCustomerQueue<Type>::isEmptyQueue() const {
     return (queueFront == nullptr);
 }
 
-// 2. التحقق لو الطابور مليان (في الـ Linked List عمره ما بيكون مليان)
 template <class Type>
 bool WaitingCustomerQueue<Type>::isFullQueue() const {
-    return false;
+    return false; 
 }
 
-// 3. تهيئة الطابور (مسح كل العناصر اللي فيه)
 template <class Type>
 void WaitingCustomerQueue<Type>::initializeQueue() {
-    nodeType<Type>* temp;
-    while (queueFront != nullptr) {
-        temp       = queueFront;
-        queueFront = queueFront->link;
+    NodeType<Type>* temp;
+    while (queueFront  != nullptr) {
+        temp = queueFront ;
+        queueFront  = queueFront ->link;
         delete temp;
     }
     queueRear = nullptr;
 }
 
-// 4. إرجاع أول عنصر
 template <class Type>
 Type WaitingCustomerQueue<Type>::front() const {
-    assert(queueFront != nullptr);  // التأكد إن الطابور مش فاضي
+    assert(queueFront != nullptr);
     return queueFront->info;
 }
 
-// 5. إرجاع آخر عنصر
 template <class Type>
 Type WaitingCustomerQueue<Type>::back() const {
-    assert(queueRear != nullptr);  // التأكد إن الطابور مش فاضي
+    assert(queueRear != nullptr);
     return queueRear->info;
 }
 
-// 6. إضافة عنصر جديد للطابور (عميل جديد)
 template <class Type>
 void WaitingCustomerQueue<Type>::addQueue(const Type& newElement) {
-    nodeType<Type>* newNode;
-    newNode       = new nodeType<Type>;
-    newNode->info = newElement;
-    newNode->link = nullptr;  // link = nullptr للعقدة الجديدة
-
-    if (queueFront == nullptr) {  // لو الطابور فاضي
+    NodeType<Type>* newNode = new NodeType<Type>;
+    newNode->info = queue_element;
+    newNode->link = nullptr;
+    
+    if (queueFront == nullptr) {
         queueFront = newNode;
-        queueRear  = newNode;
-    } else {  // لو فيه ناس واقفة
+        queueRear = newNode;
+    } else {
         queueRear->link = newNode;
-        queueRear       = queueRear->link;
+        queueRear = newNode;
     }
 }
 
-// 7. حذف عنصر من الطابور (عميل دخل للسيرفر)
 template <class Type>
-void WaitingCustomerQueue<Type>::deleteQueue() {
-    nodeType<Type>* temp;
-    if (!isEmptyQueue()) {
-        temp       = queueFront;
+void WaitingCustomerQueue<Type>::deleteQueue(){
+    if (!is_empty_queue()) {
+        NodeType<Type>* temp = queueFront;
         queueFront = queueFront->link;
         delete temp;
-
-        if (queueFront == nullptr)  // لو مسحنا آخر عنصر
+        
+        if (queueFront == nullptr) {
             queueRear = nullptr;
+        }
     } else {
-        cout << "Cannot remove from an empty queue." << endl;
+        std::cout << "ERROR: Cannot remove from empty queue." << std::endl;
     }
 }
 
-// 8. الدالة الخاصة: تحديث وقت الانتظار لكل العملاء في الطابور
 template <class Type>
-void WaitingCustomerQueue<Type>::incrementWaitingTimes() {
-    nodeType<Type>* current = queueFront;
+void WaitingCustomerQueue<Type>::incrementWaitingTimes(){
+    NodeType<Type>* current = queueFront;
     while (current != nullptr) {
-        // بننادي على دالة زيادة وقت الانتظار اللي جوه كلاس العميل
         current->info.incrementWaitingTime();
         current = current->link;
     }
 }
 
-// 9. Constructor
 template <class Type>
 WaitingCustomerQueue<Type>::WaitingCustomerQueue() {
     queueFront = nullptr;
-    queueRear  = nullptr;
+    queueRear = nullptr;
 }
 
-// 10. Destructor
 template <class Type>
 WaitingCustomerQueue<Type>::~WaitingCustomerQueue() {
     initializeQueue();
 }
 
-// ---------------------------------------------------------
-// Explicit Template Instantiation
-// عشان الـ Linker بتاع الـ C++ ميضربش error وإحنا بنبني المشروع
 #include "CustomerType.h"
 template class WaitingCustomerQueue<CustomerType>;
