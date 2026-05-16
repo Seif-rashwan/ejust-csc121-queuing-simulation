2474dde4f49ba34318e1b3d6769bbcbff658a512
Egiziano <56704274+EgizianoEG@users.noreply.github.com>
2026-05-17 01:39:02 +0300

    fix: synchronize naming

--- include/CustomerType.h
+++ include/CustomerType.h
@@ -9,7 +9,7 @@ class CustomerType {
     int transaction_time_ = 0;
 
    public:
-    void setCustomerInfo(int customer_n = 0, int arr_time = 0, int w_time = 0, int time_spent = 0);
+    void setCustomerInfo(int customer_no = 0, int arr_time = 0, int w_time = 0, int time_spent = 0);
 
     int getWaitingTime() const;
     void setWaitingTime(int time);
@@ -19,7 +19,7 @@ class CustomerType {
     int getTransactionTime() const;
     int getCustomerNumber() const;
 
-    explicit CustomerType(int customer_n = 0, int arr_time = 0, int w_time = 0, int t_time = 0);
+    explicit CustomerType(int customer_no = 0, int arr_time = 0, int w_time = 0, int t_time = 0);
 };
 
 #endif  // INCLUDE_CUSTOMERTYPE_H_
